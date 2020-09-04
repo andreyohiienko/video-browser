3c710fc516286e3eeab611b1739f8e28d77b8447
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-04 12:38:37 +0300

    Starting from scratch

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    Hi there
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-}
+export default {}
 </script>
